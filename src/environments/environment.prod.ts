784d6406836c1908ac9a3990dac8efd87134a3f1
Tajul Islam Refath <72746776+tajul-islam-refath@users.noreply.github.com>
2022-09-06 21:35:04 +0600

    update pro.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBPbBG-HLpRNqjrWfUDnLhMgRFAkNhj97g",
+    authDomain: "task-manager-1bbd0.firebaseapp.com",
+    projectId: "task-manager-1bbd0",
+    storageBucket: "task-manager-1bbd0.appspot.com",
+    messagingSenderId: "686976832241",
+    appId: "1:686976832241:web:f46f3c9141418363600f50",
+    measurementId: "G-JM86HBQVST"
+  }
 };
